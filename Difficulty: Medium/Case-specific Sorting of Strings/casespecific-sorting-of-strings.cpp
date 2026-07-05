ad03b39e4dabdf21316ddcd51fa5113aaa2d2bb6
Jay Patel <pateljay18v@gmail.com>
2026-07-05 13:17:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Case-specific Sorting of Strings/casespecific-sorting-of-strings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    string caseSort(string& s) {
+        // code here
+        string lower , upper;
+        for(char c : s){
+            if(islower(c)) lower.push_back(c);
+            else upper.push_back(c);
+        }
+        
+        sort(lower.begin(),lower.end());
+        sort(upper.begin(),upper.end());
+        
+        string result;
+        int li=0,ui=0;
+        for(char c: s){
+            if(islower(c)) result.push_back(lower[li++]);
+            else result.push_back(upper[ui++]);
+        }
+        
+        return result;
+        
+        
+    }
+};
